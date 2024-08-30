c9645f5f36eb110cc915792ed728570096f94e62
herculehm <92490723+herculehm@users.noreply.github.com>
2024-08-31 02:14:40 +0300

    Update music.py

--- music.py
+++ music.py
@@ -71,7 +71,7 @@ class Music(commands.Cog):
             ffmpeg_options = {
                 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5',
                 'options': '-vn'
-        }
+}
 
             try:
             # Ses dosyasını oynat
