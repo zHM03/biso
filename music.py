8a51161cd83d2b460b5657070006468ff4f08c1d
herculehm <92490723+herculehm@users.noreply.github.com>
2024-08-31 23:59:54 +0300

    Update music.py

--- music.py
+++ music.py
@@ -78,7 +78,13 @@ class Music(commands.Cog):
 
     async def after_play(self):
         if len(self.queue) > 0:
-            await self.play_next()
+            self.queue.pop(0)  # Kuyruğun ilk şarkısını çıkart
+            if len(self.queue) == 0:
+                self.is_playing = False
+                if self.voice_client and self.voice_client.is_connected():
+                    await self.voice_client.disconnect()
+            else:
+                await self.play_next()
         else:
             self.is_playing = False
             if self.voice_client and self.vocie_client.is_connected():
